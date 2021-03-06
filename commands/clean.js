2b40573d6ca294f2b6bf23a50ebd8c4135e5748b
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-03-06 19:58:07 +0200

    Create clean.js
    Added command clean for removing bot messages.

---
+++ commands/clean.js
@@ -0,0 +1,20 @@
+module.exports.run = async (bot, message, args) => {
+    var messages = await message.channel.messages.fetch({ limit: 100 })
+    for ([id, messag] of messages) {
+        if (messag.content.startsWith(bot.prefix)) {
+            try {
+                await messag.delete()
+            } catch (error) { console.error(error) }
+        } else if (messag.author.id = bot.user.id){
+            for (const [id, reaction] of messag.reactions.cache) {
+                try {
+                    await reaction.users.fetch()
+                } catch (error) { console.error(error) }
+            }
+        }
+    }
+}
+
+module.exports.help = {
+    name: "clean"
+}
