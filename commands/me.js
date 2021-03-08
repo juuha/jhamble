5a8e45f574c0607946c09b9f647435812d47b629
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-03-08 20:27:47 +0200

    Added base for luck system
    Renamed magic find to luck, and luck to exp.

--- commands/me.js
+++ commands/me.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const init_emojis = require("../functions/init_emojis.js")
 const init_gambler = require('../functions/init_gambler.js')
+const luck_tiers = require("../luck_tiers.json")
 
 module.exports.run = async (bot, message) => {
     let message_copy = message
@@ -13,13 +14,37 @@ module.exports.run = async (bot, message) => {
     let emojis = await init_emojis(bot)
     
     gambler = bot.gamblers[message_copy.author.id]
+    let info = `${emojis.gold}: ${gambler.gold}\n${emojis.ecto}: ${gambler.ecto}`
 
-    let info = `Gold - ${gambler.gold} \nEctos - ${gambler.ecto}`
+    info += `\nMagic Find :four_leaf_clover:: ${gambler.luck} \n Luck :chart_with_upwards_trend:: ${gambler.luck}/${luck_tiers[gambler.luck]}` 
 
-    if (gambler.orbs > 0) {
-        info += `\nOrbs of Crystallized Plasma - ${gambler.orbs}`
+    if (gambler.orbs) {
+        info += `\n${emojis.orb}: ${gambler.orbs}`
+    }
+
+    if (gambler.goo) {
+        info += `\n${emojis.goo}: ${gambler.goo}`
+    }
+
+    if (gambler.jhemonade) {
+        info += `\n$:tropical_drink:: ${gambler.jhemonade}`
     }
     
+    if (gambler.mystic_nexus && !gambler.mystic_forge_conduit) {
+        info += `\nMystic Nexus :dna:: Acquired!`
+    }
+
+    if (gambler.mystic_frame && !gambler.mystic_forge_conduit) {
+        info += `\nMystic Frame :film_frames:: Acquired!`
+    }
+
+    if (gambler.mystic_forge_node && !gambler.mystic_forge_conduit) {
+        info += `\nMystic Forge Node :roll_of_paper:: Acquired!`
+    }
+
+    if (gambler.mystic_forge_conduit) {
+        info += `\nMystic Forge Conduit :toilet:: Acquired!`
+    }
 
 
     const gambler_id = message_copy.author.id
