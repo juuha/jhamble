7f872532ca63bb2ae9e907ebd9cabb77ebbc78cc
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-03-23 23:56:59 +0200

    Refactored buy to clarify what is happening

--- commands/buy.js
+++ commands/buy.js
@@ -8,36 +8,23 @@ module.exports.run = async (bot, message, args) => {
         message.delete()
     } catch (error) { console.log(error) }
 
-    let what = ""
-    let amount = 0
+    let amount = parseInt(args[0])
     let error_message = ""
 
     let gambler = await init_gambler(bot, message_copy.author)
     let emojis = await init_emojis(bot)
-    let price = { "ecto": 0.4 }
+    let price = 0.4
 
-    if (Number.isInteger(parseInt(args[0]))) {
-        amount = parseInt(args[0])
-        what = args[1]
-        if (amount <= 0) {
-            error_message = "Amount can't be less than 1."
-        } else {
-            if (what) {
-                items = ["ecto", "ectos"]
-                if (!items.includes(what)) {
-                    error_message = `${what} is not for sale!`
-                } else {
-                    if (what == "ectos") { what = "ecto" }
-                    if (gambler.gold < amount * price[what]) {
-                        error_message = `${amount} ${emojis[what]} would cost ${amount * price[what]} ${emojis.gold}. You only have ${gambler.gold} ${emojis.gold}.`
-                    }
-                }
-            } else {
-                error_message = `Specify what you want to buy!`
+    if (amount) {
+        if (amount > 0) {
+            if (gambler.gold < amount * price) {
+                error_message = `${amount} ${emojis.ecto} would cost ${amount * price} ${emojis.gold}. You only have ${gambler.gold} ${emojis.gold}.`
             }
+        } else {
+            error_message = "Amount can't be less than 1."
         }
     } else {
-        error_message = `Usage: ${bot.prefix}buy <amount> ectos`
+        error_message = `Usage: ${bot.prefix}buy <amount>`
     }
 
     if (error_message) {
@@ -48,13 +35,13 @@ module.exports.run = async (bot, message, args) => {
         return
     }
 
-    gambler[what] += amount
-    gambler.gold -= amount * price[what]
+    gambler.ecto += amount
+    gambler.gold -= amount * price
 
     await update_gambler(gambler)
 
     try {
-        const sent = await message_copy.channel.send(`${amount} ${emojis.ecto} bought for ${amount * price[what]} ${emojis.gold}.`)
+        const sent = await message_copy.channel.send(`${amount} ${emojis.ecto} bought for ${amount * price} ${emojis.gold}.`)
         await sent.delete({ timeout: 10000 })
     } catch (error) { console.log(error) }
     return
