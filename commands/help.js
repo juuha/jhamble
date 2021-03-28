743e41311e0a7fcba5d16c8a851364e77b53b4e3
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-03-28 17:41:09 +0300

    removed non-generic message from help

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message) => {
     const embed = new Discord.MessageEmbed()
         .setTitle(`The name's Jhamble, ready to gamble!`)
         .setColor(0xFF0000)
-        .setDescription(`**${bot.prefix}gamble <amount>** - Ectogambles the declared amount. Amount can be left empty if only gambling once. \n **${bot.prefix}me** - Shows information about yourself.\n **${bot.prefix}buy <amount>** - Used for buying ectos if you have too much gold and too few ectos.\n**${bot.prefix}sell <amount> <what>** - You can sell ectos and orbs if you have too many of those and too little gold. \n**${bot.prefix}give <@who> <amount> <what>** - If your friend has too little of something, you can give them something. (ectos, orbs, gold, jhemonade)\n**${bot.prefix}craft <amount>** - Crafts 1 jhemonade using 50 orbs!\n\n Give Jhem a jhemonade for a free ecto gamble in gw2.`)
+        .setDescription(`**${bot.prefix}gamble <amount>** - Ectogambles the declared amount. Amount can be left empty if only gambling once. \n **${bot.prefix}me** - Shows information about yourself.\n **${bot.prefix}buy <amount>** - Used for buying ectos if you have too much gold and too few ectos.\n**${bot.prefix}sell <amount> <what>** - You can sell ectos and orbs if you have too many of those and too little gold. \n**${bot.prefix}give <@who> <amount> <what>** - If your friend has too little of something, you can give them something. (ectos, orbs, gold, jhemonade)\n**${bot.prefix}craft <amount>** - Crafts 1 jhemonade using 50 orbs!`)
     try {
         await dm.send(embed)
     } catch (error) { console.error(error) }
