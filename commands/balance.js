00a1bd1da111e4648e02827f90d9e9326baf6d1a
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-04-07 17:32:38 +0300

    Added balance command
    - Balance evens gold and ecto amount of the gambler, so their values are even.
    - also updated help message

---
+++ commands/balance.js
@@ -0,0 +1,41 @@
+const init_emojis = require("../functions/init_emojis")
+const init_gambler = require("../functions/init_gambler")
+
+module.exports.run = async (bot, message) => {
+    let message_copy = message
+    try {
+        message.delete()
+    } catch (error) { console.log(error) }
+
+    let gambler = await init_gambler(bot, message_copy.author)
+    let emojis = await init_emojis(bot)
+    let ecto = gambler.ecto
+
+    let total = gambler.gold + gambler.ecto * 0.4
+    gambler.gold = total / 2
+    gambler.ecto = total / (2 * 0.4)
+
+    let delta_ecto = gambler.ecto - ecto
+    let new_message = ""
+
+    if (delta_ecto < 0) {
+        new_message = `Sold ${-1 * delta_ecto}${emojis.ecto} for ${-0.4 * delta_ecto}${emojis.gold}!`
+    } else if (delta_ecto > 0) {
+        new_message = `Bought ${delta_ecto}${emojis.ecto} for ${0.4 * delta_ecto}${emojis.gold}!`
+    } else {
+        new_message = "Already perfectly balanced, as all things should be."
+    }
+
+    try {
+        let sent = await message_copy.channel.send(new_message)
+        try {
+            sent.delete({ timeout: 10000 })
+        } catch (error) { console.log(error) }
+    } catch (error) { console.log(error) }
+}
+
+module.exports.help = {
+    name: "balance",
+    short: "bal",
+    alt: "even"
+}
