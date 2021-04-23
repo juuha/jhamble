6273686e81da97466f46865f87771b65b8b4e06f
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-04-23 19:30:43 +0300

    Updated init_emojis to new standard and added emoji for balance

--- functions/init_emojis.js
+++ functions/init_emojis.js
@@ -3,31 +3,32 @@ const Discord = require('discord.js')
 module.exports = async (bot) => {
     let emojis = {}
 
-    emojis.ecto = ":game_die:"
+    emojis.ecto = "🎲"
     const ecto_emoji = bot.emojis.cache.find(emoji => emoji.name === 'ecto')
     if (ecto_emoji) emojis.ecto = ecto_emoji
     
-    emojis.gold = ":medal:"
+    emojis.gold = "🏅"
     const gold_emoji = bot.emojis.cache.find(emoji => emoji.name === 'gold')
     if (gold_emoji) emojis.gold = gold_emoji
 
-    emojis.orb = ":crystal_ball:"
+    emojis.orb = "🔮"
     const orb_emoji = bot.emojis.cache.find(emoji => emoji.name === "orb")
     if (orb_emoji) emojis.orb = orb_emoji
 
-    emojis.crystal = ":gem:"
+    emojis.crystal = "💎"
     const crystal_emoji = bot.emojis.cache.find(emoji => emoji.name === "crystal")
     if (crystal_emoji) emojis.crystal = crystal_emoji
 
-    emojis.glob = ":low_brightness:"
+    emojis.glob = "🔅"
     const glob_emoji = bot.emojis.cache.find(emoji => emoji.name === "glob")
     if (glob_emoji) emojis.glob = glob_emoji
 
-    emojis.asc_glob = ":high_brightness:"
+    emojis.asc_glob = "🔆"
     const asc_glob_emoji = bot.emojis.cache.find(emoji => emoji.name === "asc_glob")
     if (asc_glob_emoji) emojis.asc_glob = asc_glob_emoji
 
-    emojis.jhemonade = ":tropical_drink:"
+    emojis.jhemonade = "🍹"
+    emojis.balance = "⚖️"
 
     return emojis
 }
